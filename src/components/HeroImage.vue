713e8e62cfb5cd27796a923ab654aaa842aa5bce
Mike <shadowmare@windowslive.com>
2019-05-03 14:35:20 +0200

    Finished app

---
+++ src/components/HeroImage.vue
@@ -0,0 +1,34 @@
+<template>
+    <img class="heroImage"/>
+</template>
+
+<script>
+    export default {
+        name: "HeroImage"
+    }
+</script>
+
+<style lang="scss" scoped>
+  .heroImage{
+    position: fixed;
+    top: 0;
+    z-index: -1;
+    width: 100%;
+    height: 100vh;
+    background-image: url('../assets/heroimage.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 80% 0%;
+  }
+.heroImage::after {
+  content: '';
+  background: black;
+  opacity: 0.1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  width: 100%;
+  height: 100vh;
+}
+</style>
--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,34 +1,62 @@
 <template>
-  <div class="searchWrapper">
-    <label for="search">Search</label>
     <input
       id="search"
       name="search"
-      v-model="searchValue"
-      @input="handleInput"
+      :class="{ dark }"
+      :value="value"
+      @input="handleChange"
       />
-  </div>
 </template>
 <script>
   export default {
     name: "SearchInput",
+    props: {
+      value: {
+        type: String,
+        required: true,
+      },
+      dark: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    methods: {
+      handleChange(e){
+  this.$emit('input', e.target.value);
+      },
+    },
   };
 </script>
   <style lang="scss" scoped>
-    .searchWrapper{
-      margin-top: 50px;
-      display: flex;
-      flex-direction: column;
-      width: 250px;
 
-    label{
-      font-family: Montserrat, sans-serif;
-    }
   input{
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+    width: 250px;
+    color: white;
+    text-align: center;
+    font-size: 18px;
+    font-weight: 300;
     height: 30px;
     border: 0;
     background: none;
-    border-bottom: 1px solid black;
-  }
+    border-bottom: 1px solid white;
+    transition: box-shadow .3s ease-out;
+    @media (min-width: 1024px){
+      font-weight: 400;
     }
+  }
+  input:focus {
+    outline: none;
+    box-shadow: 0px 10px 20px -8px rgba(255,255,255, .5);
+  }
+  .dark{
+    color: #1e3d4a;
+    border-bottom-color: #1e3d4a;
+  }
+ .dark:focus {
+
+    box-shadow: 0px 10px 20px -8px rgba(#1e3d4a, .2);
+  }
   </style>
